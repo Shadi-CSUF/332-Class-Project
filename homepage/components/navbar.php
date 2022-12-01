ca3e8a998976e02a608d169ec923b251f60641de
Jimmy Quach <jminquach@gmail.com>
2022-11-30 23:55:18 -0800

    remove login component

--- homepage/components/navbar.php
+++ homepage/components/navbar.php
@@ -16,9 +16,6 @@
         <a class="nav-link" href="javascript:;" onclick="openStudentView()">Student View</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
-    </form>
   </div>
 </nav>
 
